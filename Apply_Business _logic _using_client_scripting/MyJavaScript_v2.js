7c1a570731caee5dc5bc07f6d41c806049bcdbc4
Jean-Paul Dosher <jpdosher@hotmail.com>
2023-10-01 12:54:40 -0300

    Update MyJavaScript_v2.js

--- Apply_Business _logic _using_client_scripting/MyJavaScript_v2.js
+++ Apply_Business _logic _using_client_scripting/MyJavaScript_v2.js
@@ -1,13 +1,14 @@
-this.formOnLoad = function(executionContext) {
+this.formOnLoad = function(executionContext) 
+{
     var formContext = executionContext.getFormContext();
     formContext.ui.setFormNotification("This is a notification v2", "INFO","IDUnique011023");    
-    if(formContext.getAttribute("telephone1").getValue() == null)
+    if (formContext.getAttribute("telephone1").getValue() == null)
     {
-        formContext.getAttribute("telephone1").setValue("123-4567");
+        formContext.getAttribute("telephone1").setValue("123-456-7890");
         formContext.getControl("telephone1").addNotification({
-            messages: ["Valor de Telefone atribuído com valor padrão."],
-            notificationLevel: "RECOMENDATION",
-            uniqueID: "IDUnique011023-2"
+            messages: ["Valor de Telefone foi atribuído com valor padrão!"],
+            notificationLevel: "RECOMMENDATION",
+            uniqueId: "IDUnique011023-2"
         })
 
     }
