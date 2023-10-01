ca652657204f3e1e9562b482c1c808411f8037a1
Jean-Paul Dosher <jpdosher@hotmail.com>
2023-10-01 10:20:49 -0300

    Add notification in Control "telephone1"

--- Apply_Business _logic _using_client_scripting/MyJavaScript_v2.js
+++ Apply_Business _logic _using_client_scripting/MyJavaScript_v2.js
@@ -1,8 +1,15 @@
 this.formOnLoad = function(executionContext) {
     var formContext = executionContext.getFormContext();
     formContext.ui.setFormNotification("This is a notification v2", "INFO","IDUnique011023");    
-    if(formContext.getAttribute("telephone1").getValue() == null){
+    if(formContext.getAttribute("telephone1").getValue() == null)
+    {
         formContext.getAttribute("telephone1").setValue("123-4567");
+        formContext.getControl("telephone1").addNotification({
+            messages: ["Valor de Telefone atribuído com valor padrão."],
+            notificationLevel: "RECOMENDATION",
+            uniqueID: "IDUnique011023-2"
+        })
+
     }
     
 
