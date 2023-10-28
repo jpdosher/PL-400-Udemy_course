aea827cc765a556ed20fba114c94b4d3e6b5bd23
Jean-Paul Dosher <jpdosher@hotmail.com>
2023-10-28 17:33:20 -0300

    Update PracticeActivityNumber1.js
    Final

--- Apply_Business _logic _using_client_scripting/PracticeActivityNumber1.js
+++ Apply_Business _logic _using_client_scripting/PracticeActivityNumber1.js
@@ -1,16 +1,23 @@
 this.formOnLoad = function(executionContext) 
 {
     var formContext = executionContext.getFormContext();
-    formContext.ui.setFormNotification("This is a notification for Practice Activity Number 1", "INFO","IDUnique011023");    
-    if (formContext.getAttribute("adrress1_County").getValue() == Brasil)
-    {
-        formContext.getAttribute("telephone1").setValue("123-456-7890");
-        formContext.getControl("telephone1").addNotification({
-            messages: ["Valor de Telefone foi atribuído com valor padrão!"],
-            notificationLevel: "RECOMMENDATION",
-            uniqueId: "IDUnique011023-2"
-        })
-
-    }
-    
+    formContext.ui.setFormNotification("This is a notification for Practice Activity Number 1", "INFO","IDUnique011023");       
 }
+
+this.ChangeZipPostalCode = function (executionContext)
+{
+    var formContext = executionContext.getFormContext();
+    if (formContext.getAttribute("address1_country").getValue() == "United Kingdom")   
+    {
+        formContext.getControl("address1_composite_compositionLinkControl_address1_postalcode").setLabel("Postal code");
+    } else if (formContext.getAttribute("address1_country").getValue() == null)
+    {
+        formContext.getControl("address1_composite_compositionLinkControl_address1_postalcode").setLabel("ZIP/Postal code");
+    } else
+    {
+        formContext.getControl("address1_composite_compositionLinkControl_address1_postalcode").setLabel("ZIP code");
+    }   
+}
+
+
+
